172731bb8dfae2614b21e660091f9cd011052d43
Sergey Rybalko <Rybalko.Sergey.s@gmail.com>
2015-11-06 19:54:04 +0200

    Create application

---
+++ task1/HypnoNerd/HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Sergey Rybalko on 11/6/15.
+//  Copyright © 2015 Sergey Rybalko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ task1/HypnoNerd/HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Sergey Rybalko on 11/6/15.
+//  Copyright © 2015 Sergey Rybalko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
